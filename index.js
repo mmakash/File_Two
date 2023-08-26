64196177a796660ae0f19dffb407daeae3f1c291
mmakash <musaddekurakash@gmail.com>
2023-08-26 19:43:33 +0600

    initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const http = require("http");
+const fs = require("fs");
+const url = require("url");
+const server = http.createServer((req,res) =>{
+
+})
+server.listen(5000,() =>{
+    console.log("Server is runnig");
+})
